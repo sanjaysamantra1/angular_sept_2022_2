c2b1eeb7e6d0814cf4c4d9c950d9f6044cb51979
Sanjay Samantra <sanjaysamantra1@gmail.com>
2022-10-19 20:41:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CenterComponent } from './center/center.component';
+import { Demo1Component } from './demo1/demo1.component';
+import { CardComponent } from './card/card.component';
+import { EmployeeCrudComponent } from './employee-crud/employee-crud.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CenterComponent,
+    Demo1Component,
+    CardComponent,
+    EmployeeCrudComponent,
+    PipesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employee-crud/employee-crud.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-employee-crud',
+  templateUrl: './employee-crud.component.html',
+  styleUrls: ['./employee-crud.component.css'],
+})
+export class EmployeeCrudComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  employees = [
+    { eId: 101, name: 'sanjay', sal: 5000 },
+    { eId: 104, name: 'deepak', sal: 8000 },
+    { eId: 103, name: 'ranjan', sal: 7000 },
+    { eId: 102, name: 'manoj', sal: 9000 },
+  ];
+
+  addEmployee() {
+    Swal.fire('Good job!', 'Employee Added Successfully!', 'success');
+  }
+  editEmployee() {}
+  deleteEmployee() {
+    Swal.fire({
+      title: 'Do you want to Delete Employee?',
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Delete',
+      denyButtonText: `Don't Delete`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire('Deleted!', '', 'success');
+      } else if (result.isDenied) {
+        Swal.fire('Not Deleted', '', 'info');
+      }
+    });
+  }
+
+  selectedEmployee:any;
+  displayStyle = 'none';
+  openPopup(emp:any) {
+    this.selectedEmployee = emp;
+    this.displayStyle = 'block';
+  }
+  closePopup() {
+    this.displayStyle = 'none';
+  }
+}
